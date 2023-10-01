a132c0165b877c801ad05664fae38dd205d8decd
tssuru <115495515+tssuru@users.noreply.github.com>
2023-10-01 22:59:39 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace HW_GeoObj_3
+{
+    class GeoObj
+    {
+        private double X, Y;
+        private string _name;
+        private string _description;
+
+        public GeoObj(double x, double y, string name, string description)
+        {
+            X = x;
+            Y = y;
+            _name = name;
+            _description = description;
+        }
+
+        public virtual string GetInfo()
+        {
+            return $"Name: {_name}\n\tCoords: X - {X} / Y - {Y};\n\tDescription: {_description}\n";
+        }
+    }
+
+    class River : GeoObj
+    {
+        double _speed;
+        double _length;
+        public River(double x, double y, double speed, double length, string name, string description) : base(x, y, name, description)
+        {
+            _length = length;
+            _speed = speed;
+        }
+
+        public override string GetInfo()
+        {
+            return base.GetInfo() + "\t" + "Speed: " + _speed + "\n\t" + "Length: " + _length;
+        }
+
+
+    }
+
+    class Mountain : GeoObj
+    {
+        double _highestPoint;
+        public Mountain(double x, double y, double highestPoint, string name, string description) : base(x, y, name, description)
+        {
+            _highestPoint = highestPoint;
+        }
+
+        public override string GetInfo()
+        {
+            return base.GetInfo() + "\t" + "Highest point: " + _highestPoint;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HW_GeoObj_3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        GeoObj obj = new GeoObj(1.1, 2.2, "Object", "Some description");
+        River amazon = new River(-2.16, -55.12, 1.5, 6400, "Amazon", "The Amazon River is the largest river in South America and the largest river in the world.");
+        Mountain everest = new Mountain(27.98, 86.92, 8850, "Everest", "Mount Everest is a peak in the Himalaya mountain range.It is located between Nepal and Tibet, an autonomous region of China.");
+        Console.WriteLine(obj.GetInfo());
+        Console.WriteLine(amazon.GetInfo());
+        Console.WriteLine(everest.GetInfo());
+    }
+}
+
